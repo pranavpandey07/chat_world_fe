d6420a747c81efb780e045204416668baade4636
pranavpandey07 <pandey.pranav8@gmail.com>
2024-08-01 14:53:49 +0530

    router changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import SignupPage from './pages/SignupPage';
 import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
